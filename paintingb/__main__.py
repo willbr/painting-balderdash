7dfc32f411d073e5810d02f18ddc422748ec0d43
William Bettridge-Radford <william.bettridge.radford@gail.com>
2023-03-26 10:52:30 +0100

    tweak colours

--- paintingb/__main__.py
+++ paintingb/__main__.py
@@ -14,7 +14,7 @@ hscrollbar.pack(side=BOTTOM, fill=X)
 vscrollbar = Scrollbar(frame, orient=VERTICAL)
 vscrollbar.pack(side=RIGHT, fill=Y)
 
-canvas = Canvas(frame, bd=0, bg='#aaaaaa', xscrollcommand=hscrollbar.set, yscrollcommand=vscrollbar.set)
+canvas = Canvas(frame, bd=0, bg='#aaaaaa') #, xscrollcommand=hscrollbar.set, yscrollcommand=vscrollbar.set)
 canvas.pack(side=LEFT, fill=BOTH, expand=YES)
 
 def paint(event):
@@ -22,7 +22,7 @@ def paint(event):
     step = size / 2
     x1, y1 = (event.x - step), (event.y - step)
     x2, y2 = (event.x + step), (event.y + step)
-    canvas.create_oval(x1, y1, x2, y2, fill='black', outline='black', width=0)
+    canvas.create_oval(x1, y1, x2, y2, fill='#8B88EF',  width=0)
 
 canvas.bind('<Configure>', lambda event: canvas.configure(scrollregion=canvas.bbox('all')))
 canvas.bind('<B1-Motion>', paint)
